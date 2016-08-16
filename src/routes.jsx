f1c7ccdbe79f05251c45cc988fb50f0e922f7da7
KenYA <tikka0910@gmail.com>
2016-08-16 18:15:27 +0800

    add todo route

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,13 @@ import {Route, IndexRoute} from 'react-router';
 import {App} from 'App.jsx';
 import {CounterPage} from 'Counter/CounterPage.jsx';
 import {AboutPage} from 'About/AboutPage.jsx';
+import {TodoIndex} from 'Todo/TodoIndex.jsx';
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={CounterPage}/>
     <Route path='counter' component={CounterPage}/>
     <Route path='about/:yoo' component={AboutPage}/>
+    <Route path='todo' component={TodoIndex}/>
   </Route>
 );
